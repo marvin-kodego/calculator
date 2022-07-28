59e09b6bb8da243d67ce93c0768392bef91a5277
Marvin <marvin@kodego.ph>
2022-07-28 20:34:51 +0800

    Create v1

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let firstNumber;
 let secondNumber;
 let operation;
-let sum;
+let display = document.getElementById("display");
 
 console.log(firstNumber, operation, secondNumber);
 
-function hello() {
-    console.log("Hello, World!");
-}
+let clearMemoryButton = document.getElementById("clearMemoryButton");
+clearMemoryButton.addEventListener("click", clearMemory);
 
 function clickNumber(input) {
     if (operation === undefined) {
@@ -16,12 +15,14 @@ function clickNumber(input) {
         } else {
             firstNumber = String(firstNumber) + String(input);
         }
+        updateDisplay(firstNumber);
     } else {
         if (secondNumber === undefined) {
             secondNumber = String(input);
         } else {
             secondNumber = String(secondNumber) + String(input);
         }
+        updateDisplay(secondNumber);
     }
 
     console.log(firstNumber, operation, secondNumber);
@@ -29,16 +30,24 @@ function clickNumber(input) {
 
 function clickOperation(input) {
     switch (input) {
-        case '+': {
-            operation = '+';
+        case "x": {
+            operation = "x";
             break;
         }
-        case '=': {
-            sum = Number(firstNumber) + Number(secondNumber);
-            console.log(sum);
-            firstNumber = undefined;
-            secondNumber = undefined;
-            operation = undefined;
+        case "/": {
+            operation = "/";
+            break;
+        }
+        case "+": {
+            operation = "+";
+            break;
+        }
+        case "-": {
+            operation = "-";
+            break;
+        }
+        case "=": {
+            calculate();
             break;
         }
         default: {
@@ -47,4 +56,77 @@ function clickOperation(input) {
     }
     
     console.log(firstNumber, operation, secondNumber);
+}
+
+function switchSign() {
+    if (operation === undefined) {
+        firstNumber = Number(firstNumber);
+        firstNumber = -1 * firstNumber;
+        firstNumber = String(firstNumber)
+        updateDisplay(firstNumber);
+    } else {
+        secondNumber = Number(secondNumber);
+        secondNumber = -1 * secondNumber;
+        secondNumber = String(secondNumber)
+        updateDisplay(secondNumber);
+    }
+}
+
+function switchToPercent() {
+    if (operation === undefined) {
+        firstNumber = Number(firstNumber);
+        firstNumber = firstNumber / 100;
+        firstNumber = String(firstNumber)
+        updateDisplay(firstNumber);
+    } else {
+        secondNumber = Number(secondNumber);
+        secondNumber = secondNumber / 100;
+        secondNumber = String(secondNumber)
+        updateDisplay(secondNumber);
+    }
+}
+
+function clearMemory() {
+    if (firstNumber === undefined
+        && secondNumber === undefined
+        && operation === undefined
+    ) {
+        updateDisplay(0);
+    }
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operation = undefined;
+
+    console.log(firstNumber, operation, secondNumber);
+}
+
+function calculate() {
+    let result;
+    switch (operation) {
+        case "x": {
+            result = Number(firstNumber) * Number(secondNumber);
+            break;
+        }
+        case "/": {
+            result = Number(firstNumber) / Number(secondNumber);
+            operation = "/";
+            break;
+        }
+        case "+": {
+            result = Number(firstNumber) + Number(secondNumber);
+            break;
+        }
+        case "-": {
+            result = Number(firstNumber) - Number(secondNumber);
+            break;
+        }
+    }
+    console.log(result);
+    updateDisplay(result);
+    clearMemory();
+}
+
+function updateDisplay(number) {
+    number = Number(number).toLocaleString();
+    display.innerText = number;
 }
